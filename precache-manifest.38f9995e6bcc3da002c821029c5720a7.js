c4737049b5cf5fbb34bdf303efa5d303c6a6a0da
idomusha <idomusha@soot.black>
2020-02-13 03:57:06 -0500

    Updates

---
+++ precache-manifest.38f9995e6bcc3da002c821029c5720a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02010b1c158b2cbdac3b933e99da2219",
+    "url": "/use-interaction/index.html"
+  },
+  {
+    "revision": "21fe73110f0fee12ff06",
+    "url": "/use-interaction/static/css/main.67cda517.chunk.css"
+  },
+  {
+    "revision": "6a1f7028f967b4634975",
+    "url": "/use-interaction/static/js/2.234b8681.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/use-interaction/static/js/2.234b8681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21fe73110f0fee12ff06",
+    "url": "/use-interaction/static/js/main.d12f1297.chunk.js"
+  },
+  {
+    "revision": "027a94c93c93caa168c4",
+    "url": "/use-interaction/static/js/runtime-main.fed84dac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/use-interaction/static/media/logo.5d5d9eef.svg"
+  }
+]);
